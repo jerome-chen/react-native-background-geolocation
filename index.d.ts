f3ab1243a41f9b03f22196edade73e0174b50930
Jerome Chen <jerome@secondcompany.nl>
2017-11-21 15:02:02 +0800

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+declare module 'react-native-background-geolocation' {
+  export const on: (event, successHandler, errorHandler?) => any;
+  export const un: (event, successHandler, errorHandler?) => any;
+  export const configure: (configuration, callback) => any;
+  export const start: (callback) => any;
+  export const removeListeners: () => any;
+  export const LOG_LEVEL_VERBOSE;
+}
