ec431e4b3a9b70e47ca51ee3b76a3d9bf9641d96
Jerome Chen <jerome@secondcompany.nl>
2017-12-25 12:13:34 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ declare module 'react-native-background-geolocation' {
   export const un: (event, successHandler, errorHandler?) => any;
   export const configure: (configuration, callback) => any;
   export const startSchedule: (callback: () => void) => void;
-  export const startWatchPosition: (success: (location: any) => any, failure: (errorCode: string) => any, options?: object) => any;
+  export const watchPosition: (success: (location: any) => any, failure: (errorCode: string) => any, options?: object) => any;
   export const stopWatchPosition: () => any;  
   export const getState: (callback) => void;
   export const start: (callback) => any;
