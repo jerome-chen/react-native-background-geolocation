16155a151d2289dcc084d6cc734ffe7c7557ea82
Jerome Chen <jerome@secondcompany.nl>
2018-02-07 15:03:29 +0800

    Added getLog method.

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,21 @@
 declare module 'react-native-background-geolocation' {
-  export const on: (event, successHandler, errorHandler?) => any;
-  export const un: (event, successHandler, errorHandler?) => any;
-  export const configure: (configuration, callback) => any;
-  export const startSchedule: (callback: () => void) => void;
-  export const watchPosition: (success: (location: any) => any, failure: (errorCode: string) => any, options?: object) => any;
-  export const getCurrentPosition: (callback?: () => void) => void;
-  export const stopWatchPosition: (callback?: any) => any;
-  export const getState: (callback) => void;
-  export const start: (callback) => any;
-  export const stop: () => any;
-  export const removeListeners: () => any;
-  export const LOG_LEVEL_VERBOSE;
-  export const DESIRED_ACCURACY_HIGH;
-  export const DESIRED_ACCURACY_MEDIUM;
-  export const DESIRED_ACCURACY_LOW;
-  export const DESIRED_ACCURACY_VERY_LOW;
+	export const on: (event, successHandler, errorHandler?) => any;
+	export const un: (event, successHandler, errorHandler?) => any;
+	export const configure: (configuration, callback) => any;
+	export const startSchedule: (callback: () => void) => void;
+	export const watchPosition: (success: (location: any) => any, failure: (errorCode: string) => any, options?: object) => any;
+	export const getCurrentPosition: (callback?: () => void) => void;
+	export const stopWatchPosition: (callback?: any) => any;
+	export const getState: (callback) => void;
+	export const start: (callback) => any;
+	export const stop: () => any;
+	export const removeListeners: () => any;
+	export const getLog: (callback?: (logs: string) => any) => any;
+	export const LOG_LEVEL_VERBOSE;
+	export const DESIRED_ACCURACY_HIGH;
+	export const DESIRED_ACCURACY_MEDIUM;
+	export const DESIRED_ACCURACY_LOW;
+	export const DESIRED_ACCURACY_VERY_LOW;
 }
 
 export * from './RNBackgroundGeolocation';
